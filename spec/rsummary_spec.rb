f109e7def1bc839b0dff277cfac3644b7510f449
kwyt <kashiyucar@gmail.com>
2017-03-02 18:51:35 +0900

    remove

--- spec/rsummary_spec.rb
+++ spec/rsummary_spec.rb
@@ -1,11 +1,4 @@
 require "spec_helper"
 
 RSpec.describe Rsummary do
-  it "has a version number" do
-    expect(Rsummary::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
